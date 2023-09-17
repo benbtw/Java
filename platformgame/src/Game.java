3cd92ef622c68dc783ed6001e7ebf74168c4ec1d
benbtw <58542595+benbtw@users.noreply.github.com>
2023-09-17 16:59:54 -0500

    ...

--- platformgame/src/Game.java
+++ platformgame/src/Game.java
@@ -1,2 +1,10 @@
+import javax.swing.*;
+
 public class Game {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Platformer");
+        frame.setSize(500,500);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 }
