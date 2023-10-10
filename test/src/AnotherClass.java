d03423119274acdd83d5037563307ffcea830224
benbtw <58542595+benbtw@users.noreply.github.com>
2023-10-10 17:51:47 -0500

    ....

--- test/src/AnotherClass.java
+++
@@ -1,9 +0,0 @@
-public class AnotherClass {
-    public int number = 20;
-    public void increase(){
-        number += 20;
-    }
-    public int getNumber() {
-        return number;
-    }
-}
--- test/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    static AnotherClass testing = new AnotherClass();
-    static extra ex = new extra();
-    public static void main(String[] args) {
-        ex.print();
-        testing.increase();
-        ex.print();
-    }
-}
