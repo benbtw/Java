afe0b4244aa2c7c0661a60b04fadf92e2b0b3f05
benbtw <58542595+benbtw@users.noreply.github.com>
2023-11-25 16:56:06 -0600

    ....

---
+++ SpringTest/main/src/main/java/com/program/main/model/Status.java
@@ -0,0 +1,7 @@
+package com.program.main.model;
+
+public record Status(Integer id,
+                     String userHandle,
+                     String userDisplayName,
+                     String content) {
+}
